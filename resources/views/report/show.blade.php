37bd07c54e8bf472d8a2bf2195b1707bbb118984
Dostoevskyfd <zhongli.morax.qiqi@gmail.com>
2024-10-25 19:09:59 +0500

    5 zadan

--- resources/views/report/show.blade.php
+++ resources/views/report/show.blade.php
@@ -1,17 +1,18 @@
-@extends('app.layouts')
+<!--@extends('app.layouts')
 @section('contnet')
-<form method="POST" action="{{ route('reports.update', $reports -> id) }}">
-        @csrf
+<form  action="{{ route('reports.update', $reports->id) }}" method="POST">
+@csrf
+  @method('put')
         <div class="mb-4">
         <p class="block text-gray-700 font-bold mb-2">Номер:</p>
-        <input type="text" id="number" name="number" placeholder="номер" required value="{{$report->number}}"
+        <input type="text" id="number" name="number" placeholder="номер" required value="{{$reports->number}}"
             class="border border-gray-300 rounded-lg p-2">
     </div>
     <div class="mb-4">
         <p class="block text-gray-700 font-bold mb-2">Описание:</p>
-        <textarea id="description" name="description" placeholder="описание" required class="border border-gray-300 rounded-lg p-2 ">{{$report->description}}</textarea>
+        <textarea id="description" name="description" placeholder="описание" required class="border border-gray-300 rounded-lg p-2 ">{{$reports->description}}</textarea>
     </div>
         <button type="submit" class="border-2">Обновить</button>
     </form>
 
-@endsection()
+@endsection()-->
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use  App\Http\Controllers\MainController;
 use App\Http\Controllers\ReportController;
-use App\Http\Controllers\AvtoController;
 
 Route::get('/', [MainController::class,   'showIndex'])->name('home');
 Route::get('/array', [MainController::class, 'showArray'])->name('array');
@@ -13,4 +12,4 @@ Route::delete('/reports/{report}', [ReportController::class, 'destroy'])->name('
 Route::post('/reports', [ReportController::class,'store']) -> name('reports.store');
 
 Route::get('/reports/{report}', [ReportController::class, 'show'])->name('reports.show');
-Route::put('/reports/{report}', [ReportController::class, 'update'])->name('reports.update');
+Route::put('/reports/{report}', [ReportController::class,'update'])->name('reports.update');
